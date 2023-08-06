649de6540483b5d1bfe06390c7491f3e7c14ea5c
Kevin Kobus <kevinkobus@gmail.com>
2023-08-06 16:35:33 -0600

    completed more of the front-end of capstone

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
-import "./css/styles.css"
+import "./css/style.css"
 import { UserContextProvider } from "./context/UserContext"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
