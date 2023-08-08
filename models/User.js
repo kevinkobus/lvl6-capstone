5f76a6b4f18d9a50247c268f9251acd7ca72b709
Kevin Kobus <kevinkobus@gmail.com>
2023-08-08 14:57:31 -0600

    added password encryptionto the capstone project

--- models/User.js
+++ models/User.js
@@ -43,6 +43,25 @@ const userSchema = new Schema({
   // ],
 });
 
+// pre-save hook to encrypt user passwords on signup
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) return next(err);
+    user.password = hash;
+    next();
+  });
+});
+
+// method to check encrypted password on login
+userSchema.methods.checkPassword = function (passwordAttempt, callback) {
+  bcrypt.compare(passwordAttempt, this.password, (err, isMatch) => {
+    if (err) return callback(err);
+    return callback(null, isMatch);
+  });
+};
+
 module.exports = mongoose.model("User", userSchema);
 
 // ideas - bio, player since, home course, favorite course
